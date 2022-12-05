dc90c721b13f7d45d703d10560d2b974ceba6843
mamatha-ISRO <59817698+mamatha-ISRO@users.noreply.github.com>
2022-12-04 22:51:04 -0700

    question_1 V1.1

---
+++ Mamatha_launchpad_backend_codechallenge/LaunchpadCodeChallenge.API/Controllers/EmployeesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaunchpadCodeChallenge.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        [HttpGet]
+        public void GetEmployees()
+        {
+
+        }
+    }
+}
