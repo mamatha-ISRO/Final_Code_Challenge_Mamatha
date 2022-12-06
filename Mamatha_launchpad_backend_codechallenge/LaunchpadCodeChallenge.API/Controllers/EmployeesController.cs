2da1f241589dad8dc2245098d9251406337c8e98
mamatha-ISRO <59817698+mamatha-ISRO@users.noreply.github.com>
2022-12-06 00:57:17 -0700

    Question _7  added console app and the code

--- Mamatha_launchpad_backend_codechallenge/LaunchpadCodeChallenge.API/Controllers/EmployeesController.cs
+++ Mamatha_launchpad_backend_codechallenge/LaunchpadCodeChallenge.API/Controllers/EmployeesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LaunchpadCodeChallenge.API.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LaunchpadCodeChallenge.API.Controllers
@@ -7,21 +8,36 @@ namespace LaunchpadCodeChallenge.API.Controllers
     [ApiController]
     public class EmployeesController : ControllerBase
     {
+        private readonly EmployeeManagerContext _context;
+        public EmployeesController(EmployeeManagerContext context)
+        {
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+
+
         [HttpGet]
-        public void GetEmployees()
+        public async Task<IEnumerable<Employees>> GetEmployees()
         {
-
+            var employees = await _context.Employee.ToList();
+            return Ok(employees);
         }
 
 
-        [HttpGet ("/department/{departmentId}")]
+        [HttpGet("/department/{departmentId}")]
 
-        public void GetDeptEmployees()
+        public ActionResult<>GetDeptEmployees(int departmentId)  
         {
 
+            var department = _context.Department.Find(departmentId);
+        if (Department == null)
+            {
+             return NotFound();
+               }
+        return Ok(department);
         }
 
-    }
+    
 
 }
 
---
+++ Mamatha_launchpad_backend_codechallenge/StringManipulation/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Code to iterate through 
+//list with out for each or for loop
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class QuestionClass
+{
+    public static List<string>NameList = new List<string> ()
+    {
+
+       "Jimmy",
+       "Jeffrey",
+        "John"
+    };
+
+    
+}
+class program
+{
+    static void Main(string[]args)
+    {
+        var mylist = QuestionClass.NameList;
+        mylist.ForEach(Console.WriteLine);
+    }
+}
+
